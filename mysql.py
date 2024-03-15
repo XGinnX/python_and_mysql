a24805ef52f50e30596cc55af9b4524dd1f17b96
Jean Sales <jean.sls.snts@gmail.com>
2024-03-15 15:15:27 -0300

    Adicionando modalidade de inserção

--- mysql.py
+++ mysql.py
@@ -9,8 +9,11 @@ port = 3306
 # Abre conexão com dados
 con = MySQLdb.connect(host, user, password, db, port)
 
-c = con.cursor()
+#c = con.cursor()
+c = con.cursor(MySQLdb.cursors.DictCursor) # permite que o retorno do banco venha como dicionário
 
+
+#Consultas 
 def select(campos, tabelas, where=None):
 
     global c # Permite usar o C dentro do escopo local
@@ -23,9 +26,29 @@ def select(campos, tabelas, where=None):
     
     return c.fetchall()
 
-print(select("nome, cpf","alunos","id_aluno < 5"))
+print(select("nome, cpf, data_nascimento","alunos","id_aluno < 5"))
 
+result = select("nome, cpf","alunos","id_aluno < 7")
+print(result[0])
+#print(result[0]["cpf"]) #Pega o primeiro item da lista e busca a chave "cpf"
 
+# Inserção
+def insert(valores, tabela, campos=None):
+    global c, con 
+    
+    query = "INSERT INTO " + tabela
+    if(campos):
+        query = query + "(" + campos + ") "
+    query = query + " VALUES " + ",".join(["(" + v + ")" for v in valores])
 
+    c.execute(query)
+    con.commit()
+        
+valores = [
+    "DEFAULT,'Marina Bettim', '2020-02-02','Av. Aricanga, 1521', 'São Paulo', 'SP', '12345678911','565656569'",
+    "DEFAULT,'Miguel Bettim', '2021-01-12','Av. Aricanga, 1521', 'São Paulo', 'SP', '12345658911','565651269'"
+]
 
+insert(valores, "alunos")
+print(select("nome, cpf, data_nascimento","alunos","id_aluno > 20"))
 
